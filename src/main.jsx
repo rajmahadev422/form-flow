835929bdbb2945a715a84b0b7afb0bb6f1b7ace5
rajmahadev422 <rajmahadev422@gmail.com>
2026-06-03 16:35:53 +0530

    remove strict mode from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,5 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
 )
